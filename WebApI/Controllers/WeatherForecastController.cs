9b1a5a40756ac3c4250de0588a960d30b561fe42
Khaja Moinuddin <KhajaM@fixitytech.com>
2022-11-22 15:55:11 +0530

    AddNewController

--- WebApI/Controllers/WeatherForecastController.cs
+++ WebApI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering","test" ,"Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
